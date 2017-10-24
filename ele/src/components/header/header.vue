3c887d8b11371975ca2c14cfb6a1d0a6516a735a
zhangjin <Zh.ZhangJin123@gmail.com>
2017-10-24 11:11:08 +0800

    第一次提交

---
+++ ele/src/components/header/header.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div id="div1">
+      <button class="btn btn-success">我是bootstrap的绿色按钮</button>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
+
+
